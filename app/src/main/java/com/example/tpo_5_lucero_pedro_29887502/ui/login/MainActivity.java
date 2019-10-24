4f0d8e786fd1b1782fde279d71bd312f5618c3e0
Pedro <pedro.maximilian@gmail.com>
2019-10-24 10:42:25 -0300

    Main usando ViewModel

--- app/src/main/java/com/example/tpo_5_lucero_pedro_29887502/ui/login/MainActivity.java
+++ app/src/main/java/com/example/tpo_5_lucero_pedro_29887502/ui/login/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tpo_5_lucero_pedro_29887502.R;
@@ -19,11 +20,14 @@ import com.example.tpo_5_lucero_pedro_29887502.ui.registro.RegistroActivity;
 
 public class MainActivity extends AppCompatActivity {
     private LiveDataUsuarioViewModel liveDataUsuarioViewModel;
+    private ViewModelMain viewModelMain;
 
     Usuario usuario;
 
     Button btnRegistro, btnIngresar;
     EditText etDni, etNombre, etApellido, etPass, etMail;
+    TextView tvEstado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +41,18 @@ public class MainActivity extends AppCompatActivity {
 
         usuario = new Usuario();
         liveDataUsuarioViewModel = ViewModelProviders.of(this).get(LiveDataUsuarioViewModel.class);
-
+        viewModelMain= ViewModelProviders.of(this).get(ViewModelMain.class);
         etMail = findViewById(R.id.etmail_login);
         etPass = findViewById(R.id.etpassword_login);
         btnIngresar = findViewById(R.id.btnIngresar);
+        tvEstado = findViewById(R.id.tvEstado);
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                usuario = ApiClient.login(getApplicationContext(), etMail.getText().toString(), etPass.getText().toString());
-                if (usuario==null){
-                    Toast.makeText(MainActivity.this, "Verifique Contraseña", Toast.LENGTH_LONG).show();
-                }else{
-                    Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
-                    intent.putExtra("usuario", usuario);
-                    startActivity(intent);
-                }
+
+                viewModelMain.ingresar(etMail.getText().toString(), etPass.getText().toString());
+
 
             }
         });//End btnIngresar
@@ -67,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final Observer<String> estadoObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                tvEstado.setText(s);
+            }
+        };
+
 
         final Observer<Usuario> usuarioObserverLogin = new Observer<Usuario>() {
             @Override
@@ -77,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
+        viewModelMain.getEstado().observe(this, estadoObserver);
 
         liveDataUsuarioViewModel.getUsuario().observe(this, usuarioObserverLogin);
 
--- app/src/main/java/com/example/tpo_5_lucero_pedro_29887502/ui/login/ViewModelMain.java
+++ app/src/main/java/com/example/tpo_5_lucero_pedro_29887502/ui/login/ViewModelMain.java
@@ -1,4 +1,65 @@
 package com.example.tpo_5_lucero_pedro_29887502.ui.login;
 
-public class ViewModelMain {
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.tpo_5_lucero_pedro_29887502.model.Usuario;
+import com.example.tpo_5_lucero_pedro_29887502.request.ApiClient;
+import com.example.tpo_5_lucero_pedro_29887502.ui.registro.RegistroActivity;
+
+public class ViewModelMain extends AndroidViewModel {
+
+    Usuario usuario;
+
+
+
+    Context context;
+
+    private MutableLiveData<String> estadoLiveData;
+
+    public ViewModelMain(@NonNull Application application) {
+        super(application);
+
+        context = application.getApplicationContext();
+
+    }
+
+    public void ingresar(String mail, String pass) {
+        Usuario usuario = new Usuario();
+        usuario = ApiClient.login(context, mail, pass);
+
+        if (usuario == null) {
+            setEstado("Verificar Datos Ingresados");
+        } else {
+            Intent intent = new Intent(context, RegistroActivity.class);
+            intent.putExtra("usuario", usuario);
+            context.startActivity(intent);
+        }
+    }
+
+    public LiveData<String> getEstado(){
+        if (estadoLiveData==null){
+            estadoLiveData = new MutableLiveData<String>();
+        }
+        return estadoLiveData;
+    }
+
+
+    public void setEstado(String estado){
+        if (estado!=null){
+            estadoLiveData.setValue(estado);
+        }
+
+
+    }
+
+
 }
